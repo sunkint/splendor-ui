4050b144c3504afe2bf2454ccef99b9a67a73891
ChenShiAi <1183942503@qq.com>
2020-08-12 18:07:40 +0800

    feat: Tag Update

--- docs/components/preview/TagPart.vue
+++ docs/components/preview/TagPart.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="docs-preview-part">
+    <sk-tag type="primary" size="small">small</sk-tag>
+    <sk-tag type="primary" size="middel">middel</sk-tag>
+    <sk-tag type="primary" size="large">large</sk-tag>
+    <sk-tag type="primary" round size="small">round</sk-tag>
+    <sk-tag type="primary" closable @close="close">closable</sk-tag>
+
+    <div style="margin-top:10px;"></div>
+
     <sk-tag type="primary">primary</sk-tag>
     <sk-tag type="secondary">secondary</sk-tag>
     <sk-tag type="success">success</sk-tag>
@@ -7,7 +15,18 @@
     <sk-tag type="warning">warning</sk-tag>
     <sk-tag type="info">info</sk-tag>
     <sk-tag type="light">light</sk-tag>
-    <sk-tag type="dark" size="large" :closable="true" @close="close">dark</sk-tag>
+
+    <div class="docs-preview-part" style="margin-top:10px; background-color: #eee;padding: 8px;">
+      <sk-tag type="primary" ghost>primary</sk-tag>
+      <sk-tag type="secondary" ghost>secondary</sk-tag>
+      <sk-tag type="success" ghost>success</sk-tag>
+      <sk-tag type="danger" ghost>danger</sk-tag>
+      <sk-tag type="warning" ghost>warning</sk-tag>
+      <sk-tag type="info" ghost>info</sk-tag>
+      <sk-tag type="light" ghost>light</sk-tag>
+      <sk-tag type="primary" round ghost closable @close="close">closable</sk-tag>
+    </div>
+
   </div>
 </template>
 
--- src/tag/index.tsx
+++ src/tag/index.tsx
@@ -11,7 +11,7 @@ export type TagType =
   | 'light'
   | 'dark';
 
-export type TagSize = 'small' | 'large' | 'larger';
+export type TagSize = 'small' | 'middel' | 'large';
 
 const Tag = defineComponent({
   name: 'sk-tag',
@@ -20,16 +20,18 @@ const Tag = defineComponent({
     size: String as PropType<TagSize>,
     round: Boolean,
     closable: Boolean,
+    ghost: Boolean,
   },
   setup(props, { slots, emit }) {
     return () => {
-      const { type = 'primary', size = 'small', round = false, closable = false } = props;
+      const { type = 'primary', size = 'small', round = false, closable = false, ghost = false } = props;
       const tagClass = [
         'sk-tag',
         `sk-tag-${type}`,
         `sk-tag-${size}`,
         {
           'sk-tag-round': round,
+          'sk-tag-ghost': ghost,
         },
       ];
       return (

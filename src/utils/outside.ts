045688dcdae2d91f3cb364fb75181f20802738bd
sunkint <sunkint@yeah.net>
2022-07-20 21:44:24 +0800

    fix: ssr support

--- src/utils/outside.ts
+++ src/utils/outside.ts
@@ -1,10 +1,12 @@
-import { inject, InjectionKey, provide, Ref, unref } from 'vue';
+import { inject, InjectionKey, provide, ref, Ref, unref } from 'vue';
+import isBrowser from './isBrowser';
 
 type E = Element | Ref<Element | undefined | null>;
 export const OutsideSymbol: InjectionKey<[E, E]> = Symbol();
 
 export function useClickOutside() {
-  const tupleOutsideElement = inject(OutsideSymbol, [document.body, document.body]);
+  const tupleBody = isBrowser ? ([document.body, document.body] as [E, E]) : [ref(null), ref(null)];
+  const tupleOutsideElement = inject(OutsideSymbol, tupleBody);
   return {
     clickOutside(e: MouseEvent) {
       const target = unref(tupleOutsideElement[0]) || document.body;
@@ -30,6 +32,7 @@ export function useClickOutside() {
 }
 
 export function useOutside(target: Element | Ref<Element | undefined | null>) {
-  const tupleOutsideElement = inject(OutsideSymbol, [document.body, document.body]);
+  const tupleBody = isBrowser ? ([document.body, document.body] as [E, E]) : [ref(null), ref(null)];
+  const tupleOutsideElement = inject(OutsideSymbol, tupleBody);
   provide(OutsideSymbol, [tupleOutsideElement[1], target]);
 }
